a5fc5914ceff302d9d7d94723037ea078c0f0010
Arravind Jeyamoorthy <76846213+Arravind1212@users.noreply.github.com>
2026-04-24 02:32:52 +0530

    Delete Hardware_Abstraction_Layers directory

--- Hardware_Abstraction_Layers/GPIO_driver.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPIO_DRIVER_H
-#define GPIO_DRIVER_H
-
-void GPIO_InitTempSensors(void);
-int Read_TempSensor1(void);
-int Read_TempSensor2(void);
-
-#endif // GPIO_DRIVER_H
--- Hardware_Abstraction_Layers/Timer_driver.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIMER_DRIVER_H
-#define TIMER_DRIVER_H
-
-void Timer_Init(void);
-void Timer_StartPeriodicInterrupt(void);
-
-#endif
